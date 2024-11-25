d8e98899c5aee1e4a19e6022d61755fc72dac71c
psktoure <psktoure@gmail.com>
2024-11-25 16:49:18 +0100

    update

--- test.py
+++ test.py
@@ -1,27 +1,29 @@
 from src.rhythm import fine_tune
 from src.config import DATASETS_DIR
 
-model_paths = [
-    "bert-base-uncased",
-    "psktoure/BERT_WordPiece_wikitext-103-raw-v1",
-    "psktoure/BERT_WordPiece_phonetic_wikitext-103-raw-v1",
-    "psktoure/BERT_WordPiece_phonetic_cleaned_wikitext-103-raw-v1",
-    "psktoure/BERT_BPE_phonetic_wikitext-103-raw-v1",
-    "psktoure/BERT_BPE_phonetic_cleaned_wikitext-103-raw-v1",
-]
+# model_paths = [
+#     "bert-base-uncased",
+#     "psktoure/BERT_WordPiece_wikitext-103-raw-v1",
+#     "psktoure/BERT_WordPiece_phonetic_wikitext-103-raw-v1",
+#     "psktoure/BERT_WordPiece_phonetic_cleaned_wikitext-103-raw-v1",
+#     "psktoure/BERT_BPE_phonetic_wikitext-103-raw-v1",
+#     "psktoure/BERT_BPE_phonetic_cleaned_wikitext-103-raw-v1",
+# ]
 
 
-dataset_paths = [f"{DATASETS_DIR}/verses/verses_hf"] * 2 + [f"{DATASETS_DIR}/verses/phonetic_verses_hf"] * 4
+# dataset_paths = [f"{DATASETS_DIR}/verses/verses_hf"] * 2 + [f"{DATASETS_DIR}/verses/phonetic_verses_hf"] * 4
 
 
-for model_path, dataset_path in zip(model_paths, dataset_paths):
-    fine_tune(
-        model_path=model_path, 
-        dataset_path=dataset_path, 
-        num_iterations=5,
-        batch_size=256,
-        num_epochs=3,
-        max_length=256,
-    )
+# for model_path, dataset_path in zip(model_paths, dataset_paths):
+#     fine_tune(
+#         model_path=model_path, 
+#         dataset_path=dataset_path, 
+#         num_iterations=5,
+#         batch_size=256,
+#         num_epochs=3,
+#         max_length=256,
+#     )
     
+from src.utils import download_wikitext
+download_wikitext(is_phonetic=True, phoneme=True)
 
